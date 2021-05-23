78c5f4286e8d1e107751ec8ad45cf657a168855e
Pranjal Mishra <lppranjal@gmail.com>
2021-05-23 19:40:49 +0530

    2nd commit

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-card',
@@ -7,13 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
+@Input() property:IProperty
 
-Property:any={
-  "Id":1,
-  "Name":"Birla House",
-  "Type":"House",
-  "Price":12000
-}
 
   constructor() { }
 
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import {IProperty} from 'src/app/property/IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+  properties:Array<IProperty>;
+  constructor(private housingService:HousingService) { }
 
   ngOnInit() {
+
+    this.housingService.getAllProperties().subscribe(
+      data=> {
+            this.properties=data;
+            console.log(data);
+          },
+          error=>{
+            console.log(error);
+          }
+    );
   }
+
+
+
 
 }
